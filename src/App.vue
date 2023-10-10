0834dd9aad60d734157768037d1c5f931d385699
BrunoNC22 <thebegone2@gmail.com>
2023-10-10 14:24:52 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Renderizador from './components/Renderizador.vue'
+</script>
+
+<template>
+  <Renderizador />
+</template>
+
+<style scoped></style>
---
+++ src/components/Renderizador.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+// defineProps<{ msg: string }>()
+import cytoscape from 'cytoscape';
+import { onMounted } from 'vue';
+
+import BombaDesligada from '../assets/Bomba desligada.svg'
+import Hidrometro from '../assets/Hidrometro.png'
+
+let cy
+
+function getEncodedSvg(svg: string): string {
+  return 'data:image/svg+xml;utf8,' + encodeURIComponent(svg);
+}
+
+let bombaDesl = new Image()
+
+
+function memoize(ele): any {
+  return { svg: getEncodedSvg(BombaDesligada), width: 50, height: 50 }
+}
+
+
+onMounted(() => {
+  cy = cytoscape({
+
+    container: document.getElementById('cy'), // container to render in
+
+    elements: [ // list of graph elements to start with
+      { // node a
+        data: { id: 'ligada' }
+      },
+      { // node b
+        data: { id: 'b' }
+      },
+      { // edge ab
+        data: { id: 'ab', source: 'ligada', target: 'b' }
+      }
+    ],
+
+    style: [ // the stylesheet for the graph
+      {
+        selector: 'node',
+        style: {
+          'label': 'data(id)',
+          "background-image": Hidrometro,
+          'width': 50,
+          'height': 50,
+        }
+      },
+
+      {
+        selector: 'edge',
+        style: {
+          'width': 2,
+          'line-color': '#083A5C',
+          'target-arrow-shape': 'none',
+          'curve-style': 'taxi',
+        }
+      }
+    ],
+  });
+})
+
+</script>
+
+<template>
+  <div class="container">
+    <div id="cy"></div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+#cy {
+  margin-top: 50px;
+  width: 500px;
+  height: 500px;
+  border: rgba(0, 0, 0, 0.164) solid 1px;
+  border-radius: 10px;
+}
+</style>
